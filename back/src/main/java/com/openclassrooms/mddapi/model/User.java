6bfda34a423d47a82d5a20df7b125029c56fcd80
msjemenblc <msjemenblc@gmail.com>
2025-01-05 01:47:09 +0100

    auth working with route security

--- back/src/main/java/com/openclassrooms/mddapi/model/User.java
+++ back/src/main/java/com/openclassrooms/mddapi/model/User.java
@@ -2,15 +2,15 @@ package com.openclassrooms.mddapi.model;
 
 import java.time.LocalDateTime;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.PrePersist;
-import javax.persistence.PreUpdate;
-import javax.persistence.Table;
-import javax.persistence.UniqueConstraint;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.PreUpdate;
+import jakarta.persistence.Table;
+import jakarta.persistence.UniqueConstraint;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
